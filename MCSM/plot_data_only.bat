2a84b6f98fb29d9e235749321cef951837249c57
ETOPIA Project <79306230+ETOPIA-UTwente@users.noreply.github.com>
2021-02-23 16:04:05 +0100

    Add files via upload

---
+++ MCSM/plot_data_only.bat
@@ -0,0 +1,2 @@
+
+py C:\<path_to_script>\tools\plot_sync_cmd_stream.py --device_list pi_1,pi_2  --start_time_offset 0 --test_name <test_name>
---
+++ MCSM/run_measurement.bat
@@ -0,0 +1,2 @@
+rem short measurement time 500ksps 5 seconds duration
+py C:\<path_to_script>\tools\ssh_call_stream.py --time 5 --tbc 0.2 --sr 2000 --test_name <test_name> --manual_sync 1 --stitch_files 1 --pigpio 1
